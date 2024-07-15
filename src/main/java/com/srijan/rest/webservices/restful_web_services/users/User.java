86bec8eadccb74f691b27999fced523dd5c8f111
srijan_singh <singhsrijan809@gmail.com>
2024-07-15 23:58:55 +0530

    Creating a user bean

---
+++ src/main/java/com/srijan/rest/webservices/restful_web_services/users/User.java
@@ -0,0 +1,13 @@
+package com.srijan.rest.webservices.restful_web_services.users;
+
+import java.time.LocalDate;
+
+public class User {
+	
+	private Integer id;
+	
+	private String userName;
+	
+	private LocalDate birthDate;
+
+}
