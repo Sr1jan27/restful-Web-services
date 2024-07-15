cc91fa12e82a0e4599c26d507f342a757a05c690
srijan_singh <singhsrijan809@gmail.com>
2024-07-15 07:41:24 +0530

    Hello world api

---
+++ src/main/java/com/srijan/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.srijan.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+
+}
