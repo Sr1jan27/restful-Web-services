0e3c93b7ab52dcc955b56c80da08de97de7dad8e
srijan_singh <singhsrijan809@gmail.com>
2024-07-17 07:23:37 +0530

    validations for name and dob

--- src/main/java/com/srijan/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/srijan/rest/webservices/restful_web_services/exception/CustomizedResponseEntityExceptionHandler.java
@@ -4,10 +4,16 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
 
 import com.srijan.rest.webservices.restful_web_services.users.UserNotFoundException;
 
@@ -30,6 +36,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
 		
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+				
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(),
+				"Total Errors:" + ex.getErrorCount() + " First Error:" + ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 	}
 
 }
